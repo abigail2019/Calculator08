9712370475ce02121f075ba2b7734cd695e5e1fe
pacanor <papiasb@gmail.com>
2020-01-08 16:15:10 +0200

    Feature: Added Cube interface and renamed its operation for ease of coding

---
+++ Cube/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Operations;
+
+namespace Cube
+{
+    public interface ICube
+    {
+        public dynamic Cubed(dynamic a);
+
+    }
+    public class Cube : ICube
+    {
+        public dynamic result;
+
+        public dynamic Cubed(dynamic a)
+        {
+            result = Cb.Cubed(a);
+            return result;
+        }
+    }
+}
