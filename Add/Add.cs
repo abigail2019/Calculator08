aba8550dc7f13763763b1a4b5a267e3602996e19
pacanor <papiasb@gmail.com>
2020-01-08 15:31:17 +0200

    Feature: Added addition class

---
+++ Add/Add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Operations;
+
+namespace Add
+{
+    public interface IAdd
+    {
+        public dynamic Sum(dynamic a, dynamic b);
+
+        public dynamic Sum(dynamic a);
+
+    }
+    public class Add : IAdd
+    {
+        public dynamic result;
+        public dynamic Sum(dynamic a, dynamic b)
+        {
+            result = Addition.Sum(a, b);
+            return result;
+        }
+
+        public dynamic Sum(dynamic a)
+        {
+            result = Addition.Sum(a);
+            return result;
+        }
+    }
+}
