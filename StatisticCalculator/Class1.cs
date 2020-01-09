f463add0d7b0a5d717d5d992a8037b3c22d2673c
abigail <abigailnankamba@gmail.com>
2020-01-09 11:55:20 +0200

    Feature: new statistic calcul

---
+++ StatisticCalculator/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StatisticCalculator
+{
+    public class Class1
+    {
+    }
+}
---
+++ StatisticsCalculatorTests/StatCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticsCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsCalculator.Tests
+{
+    [TestClass()]
+    public class StatCalculatorTests
+    {
+        [TestMethod()]
+        public void MeanTest()
+        {
+            StatCalculator statsCal = new StatCalculator(); //StatCalculator is a class name
+            int[] values = { 1, 2,3,4};
+            var mean = statsCal.Mean(values);
+            Assert.AreEqual(2,mean);
+        }
+    }
+}
